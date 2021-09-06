225b9a12f4cdbe85a96307b1e6428f358287e1bb
werick souza <werickxsilva@gmail.com>
2021-09-05 23:56:06 -0300

    Aprendendo JavaScript simples

---
+++ Scripts/test.js
@@ -0,0 +1,14 @@
+const count = 21314.98;
+const date = new Date("2021-09-05");
+
+function log(locale) {
+    console.log(
+        `${new Intl.DateTimeFormat(locale).format(date)} = ${new Intl.NumberFormat(locale).format(count)}`
+    );
+}
+
+log("pt-BR");
+
+log("en-US")
+
+log("de-DE");
